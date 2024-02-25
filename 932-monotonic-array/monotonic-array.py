19f0d5d6ae56a69c8abcaac2cdf7ff3ce8cf81f9
Nayan Shah <42316364+snayan06@users.noreply.github.com>
2024-02-26 00:12:41 +0530

    Time: 736 ms (52.52%) | Memory: 31.1 MB (79.39%) - LeetSync

--- 932-monotonic-array/monotonic-array.py
+++ 932-monotonic-array/monotonic-array.py
@@ -1,5 +1,20 @@
 class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
+        """
+        Check if the given list of integers is monotonic.
+
+        Parameters:
+        - nums (List[int]): List of integers to check.
+
+        Returns:
+        - bool: True if the list is monotonic (non-decreasing or non-increasing),
+                False otherwise.
+
+        Logic:
+        - Iterates through the list, setting flags for ascending and descending
+          if encountered.
+        - Returns True if the list is either entirely non-decreasing or non-increasing.
+        """
         asc_flag = False
         dsc_flag = False
         for i in range(1, len(nums)):
