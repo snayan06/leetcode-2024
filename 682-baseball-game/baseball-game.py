572c82f4d7e709185d504a04bdaf6481a71de84b
Nayan Shah <42316364+snayan06@users.noreply.github.com>
2024-02-27 00:12:59 +0530

    Time: 50 ms (29.06%) | Memory: 16.9 MB (52.59%) - LeetSync

--- 682-baseball-game/baseball-game.py
+++ 682-baseball-game/baseball-game.py
@@ -1,21 +1,33 @@
 class Solution:
     def calPoints(self, operations: List[str]) -> int:
+        """
+        Calculates the total score in a game based on a list of operations.
+
+        The game is played with a list of strings representing operations, where:
+        - "C": Represents the last valid round's score should be removed.
+        - "D": Represents doubling the last valid round's score.
+        - "+": Represents adding the last two valid round's scores.
+        - Integer: Represents a valid round's score.
+
+        The method iterates through the operations, updating the record of valid
+        scores accordingly. It then calculates and returns the total score.
+
+        Parameters:
+        - operations (List[str]): A list of strings representing the operations in the game.
+
+        Returns:
+        - int: The total score achieved in the game.
+        """
         record = []
         for opr in operations:
             if opr == "C":
                 record.pop()
             elif opr == "D":
-                record.append(record[-1]*2)
+                record.append(record[-1] * 2)
             elif opr == "+":
-                record.append(record[-1]+record[-2])
+                record.append(record[-1] + record[-2])
             else:
                 record.append(int(opr))
-        s = 0
-        for r in record:
-            s+=r
         
-        return s
-
-
-
-        
+        total_score = sum(record)
+        return total_score
