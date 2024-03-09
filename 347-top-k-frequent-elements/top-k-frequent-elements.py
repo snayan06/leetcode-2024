ea71ef785459d5221900dbb573aacd39d07cac56
Nayan Shah <42316364+snayan06@users.noreply.github.com>
2024-03-09 12:59:13 +0530

    Time: 83 ms (86.06%) | Memory: 21.2 MB (64.21%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -13,9 +13,17 @@ class Solution:
         - List[int]: The k most frequent elements in the list.
 
         Time Complexity:
-        - O(n + k*log(n)), where n is the number of unique elements in the list.
+        - Approach 1: O(n + k*log(n)), where n is the number of unique elements in the list.
           O(n) for counting occurrences using a hash map (dictionary),
           O(k*log(n)) for sorting the unique elements based on frequency.
+
+        - Approach 2: O(n + k*log(n)), where n is the number of unique elements in the list.
+          O(n) for counting occurrences using a hash map (dictionary),
+          O(k*log(n)) for maintaining a max heap of size k.
+
+        - Approach 3: O(n), where n is the number of unique elements in the list.
+          O(n) for counting occurrences using a hash map (dictionary),
+          O(n) for creating buckets and inserting elements based on frequency.
 
         Space Complexity:
         - O(n), where n is the number of unique elements in the list.
@@ -27,8 +35,27 @@ class Solution:
         for num in nums:
             c = hash_map.get(num, 0)
             hash_map[num] = c + 1
-        
-        # Sort the hash map based on frequency in descending order
-        sorted_dict = dict(sorted(hash_map.items(), key=lambda item: item[1], reverse=True))
-        
-        return list(sorted_dict.keys())[:k]
+
+        # Approach 1: Sort the hash map based on frequency in descending order
+        # sorted_dict = dict(sorted(hash_map.items(), key=lambda item: item[1], reverse=True))
+
+        # Approach 2: Use a max heap to keep track of the top k frequent elements
+        # max_heap = [(-count, num) for num, count in hash_map.items()]
+        # heapq.heapify(max_heap)
+        # result_heap = [heapq.heappop(max_heap)[1] for _ in range(k)]
+
+        # Approach 3: Bucket Sort
+        buckets = [None] * (len(nums) + 1)
+        for num, count in hash_map.items():
+            if buckets[count] is None:
+                buckets[count] = []
+            buckets[count].append(num)
+
+        result = []
+        for i in range(len(buckets) - 1, 0, -1):
+            if buckets[i] is not None:
+                result.extend(buckets[i])
+                if len(result) >= k:
+                    break
+
+        return result[:k]
