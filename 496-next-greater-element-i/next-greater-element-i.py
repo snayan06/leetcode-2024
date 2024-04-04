bd868b9c7f6237625c1b73c439786df419c22772
Nayan Shah <42316364+snayan06@users.noreply.github.com>
2024-04-04 23:44:41 +0530

    Time: 52 ms (43.55%) | Memory: 16.8 MB (45.12%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,9 +1,28 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        # create index for easier lookup
+        """
+        Find the next greater element for each element in nums1 in nums2.
+
+        Intuition:
+        - We can use a stack to find the next greater element efficiently.
+        - Create a dictionary to store the index of each element in nums1 for easier lookup.
+        - Initialize the result array with -1, which represents that there is no greater element found.
+        - Iterate through nums2.
+        - While the stack is not empty and the current element is greater than the top element of the stack:
+            - Pop elements from the stack and update the result array with the current element if the popped element is in nums1.
+        - Push the current element onto the stack.
+        - Finally, return the result array.
+
+        Time Complexity: O(N1 + N2)
+        - N1 is the length of nums1 and N2 is the length of nums2.
+        - We iterate through nums2 once, and for each element, we perform stack operations, which take O(1) time.
+
+        Space Complexity: O(N1)
+        - We use a stack and a dictionary to store indices of elements from nums1, which can take up to O(N1) space.
+        """
+        # Create index for easier lookup
         num1_index = {num: i for i, num in enumerate(nums1)}
         res = [-1] * len(nums1)
-
         stack = []
 
         for num in nums2:
