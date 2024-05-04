045545ac6f41cec504c965398d3c39df4c191559
Nayan Shah <42316364+snayan06@users.noreply.github.com>
2024-05-04 10:50:06 +0530

    Time: 54 ms (6.90%) | Memory: 16.5 MB (80.50%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
@@ -8,17 +8,30 @@ class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         answer = []
 
-        # recursion
-        def helperFunc(root):
-            # base-case
-            if not root:
-                return
+        # # recursion
+        # def helperFunc(root):
+        #     # base-case
+        #     if not root:
+        #         return
 
-            helperFunc(root.left)
-            helperFunc(root.right)
-            answer.append(root.val)
-        
-        helperFunc(root)
+        #     helperFunc(root.left)
+        #     helperFunc(root.right)
+        #     answer.append(root.val)
+
+        # helperFunc(root)
+        # return answer
+        stack = []
+        if not root:
+            return answer
+        stack.append(root)
+        while stack:
+            front_node = stack.pop()
+            answer.append(front_node.val)
+
+            if front_node.left:
+                stack.append(front_node.left)
+            if front_node.right:
+                stack.append(front_node.right)
+        # answer.reverse()
+        answer = answer[::-1]
         return answer
-
-        
