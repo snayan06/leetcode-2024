190ce7c74bb23b6d77685a6ee70286c610db03d9
Nayan Shah <42316364+snayan06@users.noreply.github.com>
2024-07-14 19:28:32 +0530

    Time: 30 ms (100.00%) | Memory: 16.7 MB (100.00%) - LeetSync

--- 3484-lexicographically-smallest-string-after-a-swap/lexicographically-smallest-string-after-a-swap.py
+++ 3484-lexicographically-smallest-string-after-a-swap/lexicographically-smallest-string-after-a-swap.py
@@ -1,22 +1,12 @@
 class Solution:
     def getSmallestString(self, s: str) -> str:
-        parity = ""
-        for char in s:
-            if int(char) % 2:
-                parity += "o"
-            else:
-                parity += "e"
-        list_of_possible_strings = [s]
         s_list = list(s)
-        print(parity)
-        for i in range(len(parity) - 1):
-            if (parity[i] == "o" and parity[i + 1] == "o") or (
-                parity[i] == "e" and parity[i + 1] == "e"
-            ):
-                copy_s_list = s_list[:]
-                copy_s_list[i], copy_s_list[i + 1] = copy_s_list[i + 1], copy_s_list[i]
-                copy_s = "".join(copy_s_list)
-                list_of_possible_strings.append(copy_s)
-        print(list_of_possible_strings)
-        list_of_possible_strings.sort()
-        return list_of_possible_strings[0]
+        n = len(s_list)
+        
+        for i in range(n - 1):
+            if (int(s_list[i]) % 2 == int(s_list[i + 1]) % 2):  # Check for "oo" or "ee"
+                if s_list[i] > s_list[i + 1]:
+                    s_list[i], s_list[i + 1] = s_list[i + 1], s_list[i]
+                    break
+
+        return "".join(s_list)
