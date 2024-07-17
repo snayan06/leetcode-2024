68c2d2b712c3d68aad0dd2c01a378f1c3000626d
Nayan Shah <42316364+snayan06@users.noreply.github.com>
2024-07-17 23:32:11 +0530

    Time: 1574 ms (66.86%) | Memory: 25.7 MB (88.99%) - LeetSync

--- 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.py
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.py
@@ -1,27 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def createBinaryTree(self, descriptions: list[list[int]]) -> Optional[TreeNode]:
-        def construct_tree(cur_node_val):
-            new_node = TreeNode(cur_node_val)
-            if cur_node_val in children_hashmap:
-                if children_hashmap[cur_node_val][0] is not None:
-                    new_node.left = construct_tree(children_hashmap[cur_node_val][0])
-                if children_hashmap[cur_node_val][1] is not None:
-                    new_node.right = construct_tree(children_hashmap[cur_node_val][1])
-            return new_node
+    def createBinaryTree(self, descriptions: List[List[int]]) -> Optional[TreeNode]:
 
-        children_set = set()
-        children_hashmap: dict[int, list[int]] = {}
+        childs = set()
+        nodes = {}
+        # creating a child nodes
+        for parent, child, isLeft in descriptions:
+            if parent not in nodes:
+                nodes[parent] = TreeNode(parent)
+            if child not in nodes:
+                nodes[child] = TreeNode(child)
 
-        for parent, child, isleft in descriptions:
-            if not parent in children_hashmap:
-                children_hashmap[parent] = [None, None]  # left and right
-            children_set.add(child)
-            target = 0 if isleft else 1
-            children_hashmap[parent][target] = child
+            childs.add(child)
 
-        for parent in children_hashmap:
-            if parent not in children_set:
-                head_node_val: int = parent
+            if isLeft == 1:
+                nodes[parent].left = nodes[child]
+            else:
+                nodes[parent].right = nodes[child]
+
+        root_node = None
+        for parent, child, isLeft in descriptions:
+            if parent not in childs:
+                root_node = nodes[parent]
                 break
-        head = construct_tree(head_node_val)
-        return head
+
+        return root_node
